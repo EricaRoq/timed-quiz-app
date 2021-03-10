49f08e12d90624738774600315f2c67d6f1bc26a
Erica Roquemore <enroquemore@gmail.com>
2021-03-10 11:19:10 -0500

    added arrays of questions and answers 1-5

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// Creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "How do you write 'Hello World' in an alert box?",
+        answer: "alert('Hello World')",
+        options: [
+            "alert('Hello World')",
+            "msgBox('Hello World')",
+            "alertBox='Hello World'",
+            "alertBox('Hello World')"
+        ]
+    },
+    {
+        numb: 2,
+        question: "How do you create a function?",
+        answer: "function myFunction()",
+        options: [
+            "function:myFunction()",
+            "function=myFunction()",
+            "function myFunction()",
+            "myFunction():function"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How does a 'for' loop start?",
+        answer: "for (i = 0; i <= 5; i++)",
+        options: [
+            "for (i = 0; i <= 5)",
+            "for (i = 0; i <= 5; i++)",
+            "for i = 1 to 5",
+            "for (i <= 5; i++)"
+        ]
+    },
+    {
+        numb: 4,
+        question: "How can you add a comment in a JavaScript?",
+        answer: "//This is a comment",
+        options: [
+            "//This is a comment",
+            "'This is a comment",
+            "<!--This is a comment-->",
+            "#This is a comment"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Commonly used data types DO NOT include",
+        answer: "alerts",
+        options: [
+            "strings",
+            "booleans",
+            "alerts",
+            "functions"
+        ]
+    },
+];
