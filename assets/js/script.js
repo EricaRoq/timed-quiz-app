84ff728a620c6afc0b8a3d2a304b8fd3427ac386
Erica Roquemore <enroquemore@gmail.com>
2021-04-05 13:12:35 -0400

    new format

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// console.log("Hello There!");
+
+// High scores modal
+// var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
+
+// Variables
+const startBtn = document.getElementById("start-btn");
