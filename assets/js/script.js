071a6d833de8fb599f201371cf79417aebbf8869
Erica Roquemore <enroquemore@gmail.com>
2021-04-07 12:29:35 -0400

    fixed timer and added questions box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,47 @@
 // console.log("Hello There!");
 
-document.addEventListener('OnStartClick', () => {
-    const timeLeftDisplay = document.querySelector('#time-left');
-    const startBtn = document.querySelector('#start-btn');
-    let timeLeft = 120;
+// document.addEventListener('OnStartClick', () => {
+//     const timer = document.querySelector('#time-left');
+//     const startBtn = document.querySelector('#start-btn');
+//     let seconds = 120;
 
-    function countDown() {
-        setInterval(function() {
-            if (timeLeft <= 0 ) {
-                clearInterval(timeLeft = 0);
-            }
-            timeLeftDisplay.innerHTML = timeLeft;
-            timeLeft -=1;
-        }, 1000)
-    }
+//     function startCount() {
+//         setInterval(function() {
+//             if (seconds <= 0 ) {
+//                 clearInterval(seconds = 0);
+//             }
+//             timer.innerHTML = seconds;
+//             seconds -= 1;
+//         }, 1000)
+//     }
 
-    startBtn.addEventListener('click', countDown);
+//     startBtn.addEventListener('click', startCount);
 
-})
+//     // Start Quiz
+//     startBtn.addEventListener('click', startQuiz);
+//     function startQuiz() {
+//         var x = document.getElementById("quiz-questions");
+//         if (x.style.display === "none") {
+//             x.style.display = "block";
+//         }else {
+//             x.style.display = "none";
+//         }
+//     }
+
+// });
+
+const startTime = 120;
+let time = startTime * 60;
+
+const countDownStart = document.getElementById('count-down');
+
+setInterval(refreshCount, 1000);
+
+function refreshCount() {
+    const mins = Math.floor(time / 120);
+    let secs = time % 120;
+
+    countDownStart.innerHTML = (mins, secs);
+    time --;
+}
 
