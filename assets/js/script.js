952246ed50a38b11fb698aea15f3171c0ff548d5
Erica Roquemore <enroquemore@gmail.com>
2021-04-07 16:35:04 -0400

    updated script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,23 +48,158 @@ function refreshCount() {
 // Starting game
 
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerEl = document.getElementById('question-container');
+const questionEl = document.getElementById('question');
+const answerButtonsEl = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    nextQuestion()
+})
 
 function startGame() {
-    console.log('Started');
     startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
     questionContainerEl.classList.remove('hide');
+    nextQuestion();
 
 }
 
 // Allowing user to see next question
 function nextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
 
+function showQuestion(question) {
+    questionEl.innerHTML = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsEl.appendChild(button);
+    });
+}
+
+function resetState() {
+    nextQuestion.classList.add('hide');
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+    }
 }
 
 // What happens when answer is selected
-function selectAnswer() {
-
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide') 
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+    
 }
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+// Questions
+const questions = [
+    {
+        question: 'How do you write "Hello World" in an alert box?',
+        answers: [
+            { text: 'alert("Hello World")', correct: true },
+            { text: 'msgBox("Hello World")', correct: false },
+            { text: 'alertBox="Hello World"', correct: false},
+            { text: 'alertBox("Hello World")', correct: false}
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+]
