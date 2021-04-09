53a24139fcfb9e1234c9176183704cedafcfb248
Erica Roquemore <enroquemore@gmail.com>
2021-04-09 13:35:39 -0400

    added local storage to modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
 const questionContainer = document.getElementById('question-container');
-const question = document.getElementById('question');
+let question = document.getElementById('question');
 const answerButtons = document.getElementById('answer-buttons');
 const countStart = document.getElementById('count-down');
 
@@ -25,14 +25,14 @@ function reloadCount() {
 // Event Listeners
 startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
-    currentQuestion++;
+    question++;
     nextQuestion();
 });
 
 
 // Starting the game
 function startGame() {
-    currentQuestion = 0;
+    question = 0;
     startButton.classList.add('hide');
     
     questionContainer.classList.remove('hide');
@@ -42,7 +42,7 @@ function startGame() {
 }
 
 // Allowing user to see next question
-function nextQuestion() {
+function Nextquestion() {
     resetState();
     showQuestion();
 }
@@ -70,8 +70,8 @@ function resetState() {
 
 // What happens when answer is selected
 function selectAnswer(x) {
-    const selectedButton = x.target;
-    const correct = selectedButton.dataset.correct;
+    const selectedBtn = x.target;
+    const correct = selectedBtn.dataset.correct;
     setStatusClass(document.body, correct)
     Array.from(answerButtons.children).forEach(button => {
         setStatus(button, button.dataset.correct)
@@ -108,17 +108,17 @@ const questions = [
     {
         question: 'How do you create a function?',
         answers: [
-            { text: 'function:myFunction()', correct: true },
+            { text: 'function:myFunction()', correct: false },
             { text: 'function=myFunction()', correct: false },
-            { text: 'function myFunction()', correct: false },
+            { text: 'function myFunction()', correct: true },
             { text: 'myFunction():function', correct: false },
         ],
     },
     {
         question: 'How does a "for" loop start?',
         answers: [
-            { text: 'for (i = 0; i <= 5)', correct: true },
-            { text: 'for (i = 0; i <= 5; i++)', correct: false },
+            { text: 'for (i = 0; i <= 5)', correct: false },
+            { text: 'for (i = 0; i <= 5; i++)', correct: true },
             { text: 'for i = 1 to 5', correct: false },
             { text: 'for (i <= 5; i++)', correct: false },
         ],
@@ -135,17 +135,17 @@ const questions = [
     {
         question: 'Commonly used data types DO NOT include:',
         answers: [
-            { text: 'strings', correct: true },
+            { text: 'strings', correct: false },
             { text: 'booleans', correct: false },
-            { text: 'alerts', correct: false },
+            { text: 'alerts', correct: true },
             { text: 'functions', correct: false },
         ],
     },
     {
         question: 'The condition in an if/else statement is enclosed within:',
         answers: [
-            { text: 'quotes', correct: true },
-            { text: 'curly brackets', correct: false },
+            { text: 'quotes', correct: false },
+            { text: 'curly brackets', correct: true },
             { text: 'parenthesis', correct: false },
             { text: 'square brackets', correct: false },
         ],
@@ -162,8 +162,8 @@ const questions = [
     {
         question: 'In JavaScript, which of the following is a logical operator?',
         answers: [
-            { text: '|', correct: true },
-            { text: '&&', correct: false },
+            { text: '|', correct: false },
+            { text: '&&', correct: true },
             { text: '%', correct: false },
             { text: '/', correct: false },
         ],
@@ -171,19 +171,26 @@ const questions = [
     {
         question: 'Arrays in JavaScript can be used to store:',
         answers: [
-            { text: 'numbers and strings', correct: true },
+            { text: 'numbers and strings', correct: false },
             { text: 'other arrays', correct: false },
             { text: 'booleans', correct: false },
-            { text: 'all of the above', correct: false },
+            { text: 'all of the above', correct: true },
         ],
     },
     {
         question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
         answers: [
-            { text: 'JavaScript', correct: true },
+            { text: 'JavaScript', correct: false },
             { text: 'terminal/bash', correct: false },
             { text: 'for loops', correct: false },
-            { text: 'console.log', correct: false },
+            { text: 'console.log', correct: true },
         ],
     },
 ];
+
+// Adding local storage to modal
+function store() {
+    const initials = document.getElementById('autoSizingInput').value;
+    window.localStorage.setItem();
+}
+
